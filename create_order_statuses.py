1714a0b67f7fd077ec5c746aebbe6a6cc61b633a
Pedro Augusto Alves Nunes <pedro.a.alves.nunes@gmail.com>
2021-12-08 16:18:05 -0300

    Update create_order_statuses.py

--- create_order_statuses.py
+++ create_order_statuses.py
@@ -1,6 +1,9 @@
 from utils.trusted_area_etl_plugin import *
 
 if __name__=='__main__':
+    '''
+    This script will read the status table and create a new table with one row per order and each status with it's own column.
+    '''
     status = get_status()
     status.pivot_table('status_created_at', 'order_id', 'status_value', {'status_created_at':'max'})
     day = dt(2019, 1, 2)
